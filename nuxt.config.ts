e1d3167d412499620c15a2a2508bc76e0cbfd081
enzonotario <enzo.notario@gmail.com>
2025-09-14 22:13:53 -0300

    Revert "chore(config): update viewport settings for better responsiveness"
    This reverts commit b2ca580c59dcda19f5dd7598c0665ba76a93ba2c.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,8 +10,7 @@ export default defineNuxtConfig({
     head: {
       titleTemplate: '%s | comparatasas.ar',
       charset: 'utf-8',
-      viewport:
-        'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover',
+      viewport: 'width=device-width, initial-scale=1',
     },
   },
 
